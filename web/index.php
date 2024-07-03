c4cfd03ac1aef214ddfbc82a82c8065181bb9f3d
Fabriciio634 <fabriciofonseca03@gmail.com>
2024-07-03 00:06:49 -0300

    make Cowsay animal controllable by env var COWSAY_FARM_CLASS

--- web/index.php
+++ web/index.php
@@ -1,7 +1,8 @@
 <?php
-    $container->set(\Cowsayphp\AnimalInterface::class,          function() {
-    return \Cowsayphp\Farm::create(\Cowsayphp\Farm\Cow::class);
-    });
+    $container->set(\Cowsayphp\AnimalInterface::class, function() {
+        $class = '\\Cowsayphp\\Farm\\'.(getenv("COWSAY_FARM_CLASS")?:'Cow');
+        return \Cowsayphp\Farm::create($class);
+      });
 
     $app->get('/coolbeans', function(Request $request, Response $response, LoggerInterface $logger, \Cowsayphp\AnimalInterface $animal) {
     $logger->debug('letting the Cowsay library write something cool.');
